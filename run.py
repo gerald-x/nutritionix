eaddc7958f3c7bf6d0ba4b278bf73b9b53a10a78
gerald-x <geraldokolodc@gmail.com>
2024-04-21 00:16:48 +0100

    Added a decorator on the app for refreshing the jwt token before expiry implicitly with cookies

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
 from flask import Flask
-from blueprints import db, migrate, jwt
+from blueprints import db, migrate, jwt, cors
 from blueprints.auth.routes import auth
+from flask_jwt_extended import (
+    create_access_token, 
+    get_jwt,
+    get_jwt_identity,
+    set_access_cookies,
+)
+from datetime import datetime, timedelta, timezone
+
 
 
 def create_app():
@@ -10,7 +18,22 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
+    cors.init_app(app)
 
+    @app.after_request
+    def refresh_expiring_jwts(response):
+        try:
+            exp_timestamp = get_jwt()["exp"]
+            now = datetime.now(timezone.utc)
+            target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+            if target_timestamp > exp_timestamp:
+                access_token = create_access_token(identity=get_jwt_identity())
+                set_access_cookies(response, access_token)
+            return response
+        except (RuntimeError, KeyError):
+            # Case where there is not a valid JWT. Just return the original response
+            return response
+    
     app.register_blueprint(auth, url_prefix="/auth/")
     return app
 
