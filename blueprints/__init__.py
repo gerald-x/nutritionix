7d62735973946bf59e99c3397f4317ef8061ded9
gerald-x <geraldokolodc@gmail.com>
2024-04-21 00:14:33 +0100

    Added cors

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 class Base(DeclarativeBase):
   pass
@@ -11,3 +12,4 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
 jwt = JWTManager()
+cors = CORS()
