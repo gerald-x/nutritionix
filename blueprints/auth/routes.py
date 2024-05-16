e009d2304457169e49b38bc76810afd86029a50f
gerald-x <geraldokolodc@gmail.com>
2024-05-16 12:48:33 +0100

    Corrected minor error

--- blueprints/auth/routes.py
+++ blueprints/auth/routes.py
@@ -37,7 +37,7 @@ def login_view():
         return jsonify({"msg": "User doesn't exists"}), 400
     
     if not check_password_hash(user.password, password):
-        return jsonify({"msg": "Password does not match"}), 400
+        return jsonify({"msg": "Incorrect Password"}), 400
 
     access_token = create_access_token(identity=user.id)
     refresh_token = create_refresh_token(identity=user.id)
@@ -51,17 +51,22 @@ def register():
     last_name = request.json.get("last_name", "").strip()
     email = request.json.get("email", "").strip()
     password = request.json.get("password", "")
+    confirm_password = request.json.get("confirm_password", "")
 
     if (not first_name or 
         not last_name or 
         not email or 
-        not password
+        not password or
+        not confirm_password
         ):
         return jsonify({"msg": "Invalid or Missing Credentials"}), 400
     
     if not validate_email(email):
         return jsonify({"msg": "email not valid"}), 400
     
+    if not confirm_password == password:
+        return jsonify({"msg": "Passwords do not match"}), 400
+            
     user_exists = db.session.execute(db.select(User).filter_by(email=email)).scalars().first()
     if user_exists:
         return jsonify({"msg": "Email already exists"}), 400
